952842f2f41837f37a52512b6d9fe99c7d4fc1ed
raphael <raphael.soupayavalliama@gmail.com>
2024-03-08 17:29:22 +0400

    Bouton lecture pause fonctionnel.
    A faire : Funky css avec le bouton

--- static/script.js
+++ static/script.js
@@ -1,13 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var lectureButton = document.querySelector('.button-lecture');
-    var song = document.getElementById('song');
+document.addEventListener('DOMContentLoaded', function() { // event listener qui attend la page html se charger entièrement et attend que le navigateur la parse
+    var lectureButton = document.querySelector('.button-lecture'); // récupère le premier élément de classe button-lecture et stock dans lectureButton
+    var song = document.getElementById('song'); // récupère l'élément d'id "song" et stock dans song
   
-    lectureButton.addEventListener('click', function() {
+    lectureButton.addEventListener('click', function() { // check si le bouton de classe button-lecture est cliqué pour effectuer la fonction en argument
       if (song.paused) {
         song.play();
-      } else {
+        lectureButton.textContent = 'Pause';
+      } else { // si re appuie du bouton lorsque la musique joue cela va arreter la musique
         song.pause();
-        song.currentTime = 0;
+        lectureButton.textContent = 'Lecture';
       }
     });
   });
