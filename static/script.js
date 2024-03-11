26acf3f7614e1d544dd05aa8adb217cf1043cde5
raphael <raphael.soupayavalliama@gmail.com>
2024-03-11 20:15:50 +0400

    FInito pipo

--- static/script.js
+++ static/script.js
@@ -4,21 +4,7 @@ document.addEventListener("DOMContentLoaded", function() { // event listener qui
     var theme = document.getElementById("theme"); // récupère l'élément d'id "theme" et stock dans theme
 
     // récupère les textes pour mettre en raibow color
-    var texteDroite =  document.getElementsByClassName("texte-droite");
-    var texteGauche =  document.getElementsByClassName("texte-gauche");
-    var texteBas =  document.getElementsByClassName("texte-bas");
-    var categorieDroite = document.getElementsByClassName("categorie-droite");
-    var categorieGauche = document.getElementsByClassName("categorie-gauche");
-    var categorieBas = document.getElementsByClassName("categorie-bas");
-    var titreDroite = document.getElementsByClassName("titre-droite");
-    var titreGauche = document.getElementsByClassName("titre-gauche");
-    var h3 = document.getElementsByTagName("h3");
-    var h4 = document.getElementsByTagName("h4");
-    var expertise = document.getElementsByClassName("expertise");
-    var interet = document.getElementsByClassName("interet");
-    var dispo = document.getElementsByClassName("disponibilite");
-    var pdp = document.getElementById("pdp");
-
+    var rainbow = document.getElementsByClassName("rainbow");
 
     
 
@@ -29,69 +15,42 @@ document.addEventListener("DOMContentLoaded", function() { // event listener qui
         lectureButton.textContent = "Pause";
         theme.setAttribute("href", "../static/funky.css"); // passe en mode funky css
         pdp.setAttribute("src", "../images/Monstre.png"); // photo funky
-
-        applyRainbowColors(texteDroite);
-        applyRainbowColors(texteGauche);
-        applyRainbowColors(texteBas);
-        applyRainbowColors(categorieDroite);
-        applyRainbowColors(categorieGauche);
-        applyRainbowColors(categorieBas);
-        applyRainbowColors(titreDroite);
-        applyRainbowColors(titreGauche);
-        applyRainbowColors(h3);
-        applyRainbowColors(h4);
-        applyRainbowColors(expertise);
-        applyRainbowColors(interet);
-        applyRainbowColors(dispo);
-
-
-
-
+        applyRainbowColors(rainbow); // applique le funky rainbow texte
+  
       } else { 
         song.pause(); // si re appuie du bouton lorsque la musique joue cela va arreter la musique
         song.currentTime = 0; // reset la musique
         lectureButton.textContent = "Lecture";
         theme.setAttribute("href", "../static/styles.css"); // remet le css par défaut
         pdp.setAttribute("src", "../images/tete-removebg.png"); // remet photo par défaut
-
-        removeRainbowEffect(texteDroite);
-        removeRainbowEffect(texteGauche);
-        removeRainbowEffect(texteBas);
-        removeRainbowEffect(categorieDroite);
-        removeRainbowEffect(categorieGauche);
-        removeRainbowEffect(categorieBas);
-        removeRainbowEffect(titreDroite);
-        removeRainbowEffect(titreGauche);
-        removeRainbowEffect(h3);
-        removeRainbowEffect(h4);
-        removeRainbowEffect(expertise);
-        removeRainbowEffect(interet);
-        removeRainbowEffect(dispo);
+       removeRainbowEffect(rainbow); // enleve le funky rainbow texte
       }
     });
 
     function applyRainbowColors(elements) {
-        var couleurs = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#0000FF', '#4B0082', '#9400D3'];
-        var couleursIndex = 0;
+        var couleurs = ['#FF0000', '#FF7F00', '#FFFF00', '#00FF00', '#0000FF', '#4B0082', '#9400D3']; // tableau de couleur de l'arc en ciel
+        var couleursIndex = 0; // l'indice de la couleur du tableau
   
-        for (var i = 0; i < elements.length; i++) {
-          var element = elements[i];
-          var text = element.innerText;
-          var rainbowText = '';
+        for (var i = 0; i < elements.length; i++) { // pour chaque élément de la classe rainbow
+          var element = elements[i]; // récupère l'élément actuel
+          var text = element.innerText; // récupère le texte de l'élément 
+          var rainbowText = ''; // texte vide qui sera actualisé à chaque élément
   
           for (var j = 0; j < text.length; j++) {
-              rainbowText += '<span style="color:' + couleurs[couleursIndex] + '">' + text[j] + '</span>';
-              couleursIndex = (couleursIndex + 1) % couleurs.length;
+              rainbowText += '<span style="color:' + couleurs[couleursIndex] + '">' + text[j] + '</span>'; // entour le caractère choisi par des <span style="color: couleurs"...
+              couleursIndex = (couleursIndex + 1) % couleurs.length; // parcours le tableau de couleur en boucle
           }
   
-          element.innerHTML = rainbowText;
+          element.innerHTML = rainbowText; // inject dans le html
         }
       }
 
     
     function removeRainbowEffect(elements) {
-        Array.from(elements).forEach((element) => {
-          element.innerHTML = element.innerText; // enleve les span pour annuler l'effet arc en ciel
+        Array.from(elements).forEach((element) => { // crée un array a partir d'élément pour utiliser forEach avec une fonction fléchée
+          element.innerHTML = element.innerText;    // la fonction fait pour chaque élément :
+                                                    // enleve les spans en mettant dans le HTML le texte meme
+                                                    // !!! ducoup il n'y a plus dutout d'élément HTML (span par exemple) !!!
         });
     }
   });
