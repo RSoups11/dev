ca3f2991c4d971a73e07ca9b070bfabbcbef663f
raphael <raphael.soupayavalliama@gmail.com>
2024-03-25 15:40:03 +0400

    finition de la page de login

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template
 
 app = Flask("__name__", static_url_path='/static')
 
+
+@app.route("/login")
+def login () :
+    return render_template("login.html")
+
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
